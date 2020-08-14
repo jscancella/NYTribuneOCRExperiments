fa3921734d10455e882fdd1bfcabbb0ee43dbdd8
John Scancella <blacksmithforlife@gmail.com>
2020-08-14 09:52:51 -0400

    getting better at detecting just the columns

--- findText.py
+++ findText.py
@@ -17,7 +17,10 @@ array([[0, 0, 1, 0, 0],
        [0, 0, 1, 0, 0]], dtype=uint8)
 """
 #kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
-kernel = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+
+#custom kernel that is used to blend together text in the Y axis
+kernel = np.array([
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -27,12 +30,17 @@ kernel = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0]], dtype=np.uint8)
        
-def cutTopOfImage(img, lccn, debug=False):
-    print("cutting the top off the image")
-    #TODO find the last row that is mostly white and use that to calculate how much to crop
-    temp_img = np.delete(img, range(0, 900), ROW)
+def blankOutTopOfImage(img, lccn, debug=False):
+    """
+    makes the top of the image black in order to remove the newspaper title
+    """
+    print("blanking out the top off the image")
+    #TODO find the last row that is mostly white and use that to calculate how much to turn black
+    temp_img = np.copy(img)
+    for row in range(0,950):
+        temp_img[row] = np.zeros(temp_img[row].shape)
     if debug:
-        cv2.imwrite('debugFiles\%s-cropped.tiff' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-blankedout.tiff' % lccn, temp_img)
     return temp_img
 
 def convertToGrayscale(img, lccn, debug=False):
@@ -57,7 +65,8 @@ def removeNoise(img, lccn, debug=False):
     erosion followed by dilation. It is useful in removing noise
     """
     print("remove noise")
-    noiseKernel = np.array([[1, 1, 0, 1, 1],
+    noiseKernel = np.array([
+       [1, 1, 0, 1, 1],
        [1, 1, 0, 1, 1],
        [1, 1, 0, 1, 1],
        [1, 1, 0, 1, 1],
@@ -106,14 +115,15 @@ def findContours(img, lccn, debug=False):
     It then returns these areas (contours) as well as the hierarchy of the areas.
     """
     
-    temp_img = cutTopOfImage(img, lccn, debug=debug)
-    temp_img = convertToGrayscale(temp_img, lccn, debug=debug)
+    temp_img = convertToGrayscale(img, lccn, debug=debug)
     temp_img = invert(temp_img, lccn, debug=debug)
+    temp_img = blankOutTopOfImage(temp_img, lccn, debug=debug)
     #temp_img = findEdges(temp_img, lccn, debug=debug)
     temp_img = dilateDirection(temp_img, lccn, debug=debug)
     temp_img = closeDirection(temp_img, lccn, debug=debug)
     temp_img = removeNoise(temp_img, lccn, debug=debug)
 
+#TODO use RETR_LIST instead of RETR_TREE so we don't waste time building a hierarchy
     contours, hierarchy = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return contours, hierarchy
 
@@ -121,28 +131,36 @@ def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
     """
     creates a bunch of tiles that are boxes around the found contours
     """
-    boxed = None
+    print("creating cropped images")
+    boxed = np.copy(img)
     green = (0, 255, 0)
+    lineThickness = 3
+    padding = 10 #padding to add around the found contour
+    
     for component in zip(contours, hierarchy[0]):
         contour = component[0]
         currentHierarchy = component[1]
         x,y,w,h = cv2.boundingRect(contour)
         if currentHierarchy[3] < 0: #outer most contour
-            if h > 20 and w > 20:
-                filepath = os.path.join(directory, '%s_x%s_y%s_w%s_h%s.tiff' % (lccn, x,y,w,h))
-                print("writing out cropped image: [%s]", filepath)
+            if h > 100 and w > 450:
+                filepath = os.path.join(directory, '%s_x%s_y%s_w%s_h%s-next%s_prev%s_child%s_parent%s.tiff' % (lccn, x,y,w,h, currentHierarchy[0], currentHierarchy[1], currentHierarchy[2], currentHierarchy[3]))
+                ystart = max(y-padding, 0)
+                yend = min(y+h+padding, img.shape[0])
+                xstart = max(x-padding, 0)
+                xend = min(x+w+padding, img.shape[1])
+                crop_img = img[ystart:yend, xstart:xend]
                 if not os.path.exists(filepath):
-                    crop_img = img[y:y+h, x:x+w]
+                    print("writing out cropped image: [%s]", filepath)
                     cv2.imwrite(filepath, crop_img)
                 if debug:
-                    if boxed is None:
-                        boxed = img.copy()
-                    cv2.rectangle(boxed,(x,y),(x+w,y+h), green, 3)
+                    #draw this specific bounding box on the copy of the image
+                    cv2.rectangle(boxed,(xstart,ystart),(xend,yend), green, lineThickness)
+                    
 
     if debug:
         filepath = os.path.join(directory, '%s-contours.tiff' % lccn)
-        if not os.path.exists(filepath):
-            cv2.imwrite(filepath, boxed)
+        #cv2.drawContours(boxed, contours, -1, green, lineThickness) #use this to draw all found contours
+        cv2.imwrite(filepath, boxed)
 
 if __name__ == "__main__":
     print("reading test image")
