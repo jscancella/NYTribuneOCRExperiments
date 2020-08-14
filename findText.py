15d89b821d89b7036a6dda037959dbe018002826
John Scancella <blacksmithforlife@gmail.com>
2020-08-13 20:11:02 -0400

    working example

--- findText.py
+++ findText.py
@@ -1,42 +1,103 @@
-import cv2
 import os
+os.environ['OPENCV_IO_ENABLE_JASPER']='True' #has to be set before importing cv2 otherwise it won't read the variable
+import cv2
+import numpy as np 
 
-kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,1))
+#for numpy array methods
+ROW = 0
+COLUMN = 1
+
+"""
+# Cross-shaped Kernel
+>>> cv.getStructuringElement(cv.MORPH_CROSS,(5,5))
+array([[0, 0, 1, 0, 0],
+       [0, 0, 1, 0, 0],
+       [1, 1, 1, 1, 1],
+       [0, 0, 1, 0, 0],
+       [0, 0, 1, 0, 0]], dtype=uint8)
+"""
+#kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
+kernel = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 0, 0, 0, 0]], dtype=np.uint8)
+       
+def cutTopOfImage(img, lccn, debug=False):
+    print("cutting the top off the image")
+    #TODO find the last row that is mostly white and use that to calculate how much to crop
+    temp_img = np.delete(img, range(0, 900), ROW)
+    if debug:
+        cv2.imwrite('debugFiles\%s-cropped.tiff' % lccn, temp_img)
+    return temp_img
 
 def convertToGrayscale(img, lccn, debug=False):
     print("converting to greyscale")
     temp_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     if debug:
-        cv2.imwrite('%s-invert.jp2' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-invert.tiff' % lccn, temp_img)
     return temp_img
 
 def invert(img, lccn, debug=False):
+    """
+    Black becomes white in the image
+    """
     print("invert image")
     _,temp_img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY_INV)
     if debug:
-        cv2.imwrite('%s-invert.jp2' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-invert.tiff' % lccn, temp_img)
+    return temp_img
+    
+def removeNoise(img, lccn, debug=False):
+    """
+    erosion followed by dilation. It is useful in removing noise
+    """
+    print("remove noise")
+    noiseKernel = np.array([[1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1]], dtype=np.uint8)
+    temp_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, noiseKernel)
+    if debug:
+        cv2.imwrite('debugFiles\%s-removeNoise.tiff' % lccn, temp_img)
     return temp_img
 
 def findEdges(img, lccn, debug=False):
     print("get edges")
     temp_img = cv2.Canny(img, 30, 200)
     if debug:
-        cv2.imwrite('%s-canny.jp2' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-canny.tiff' % lccn, temp_img)
     return temp_img
 
-def dilateXDirection(img, lccn, debug=False):
+def dilateDirection(img, lccn, debug=False):
+    """
+    It is just opposite of erosion. Here, a pixel element is '1' if atleast one pixel under the kernel is '1'. 
+    So it increases the white region in the image or size of foreground object increases. 
+    Normally, in cases like noise removal, erosion is followed by dilation. 
+    Because, erosion removes white noises, but it also shrinks our object. 
+    So we dilate it. Since noise is gone, they won't come back, but our object area increases. 
+    It is also useful in joining broken parts of an object. 
+    """
     print("applying dilation morph")
-    temp_img = cv2.dilate(img, kernel, iterations=6)
+    temp_img = cv2.dilate(img, kernel, iterations=10)
    
     if debug:
-        cv2.imwrite('%s-dilation.jp2' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-dilation.tiff' % lccn, temp_img)
     return temp_img
 
-def closeXDirection(img, lccn, debug=False):
+def closeDirection(img, lccn, debug=False):
+    """
+    Dilation followed by Erosion. It is useful in closing small holes inside the foreground objects, or small black points on the object
+    """
     print("applying closing morph")
     temp_img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
     if debug:
-        cv2.imwrite('%s-close.jp2' % lccn, temp_img)
+        cv2.imwrite('debugFiles\%s-close.tiff' % lccn, temp_img)
     return temp_img
 
 def findContours(img, lccn, debug=False):
@@ -45,13 +106,15 @@ def findContours(img, lccn, debug=False):
     It then returns these areas (contours) as well as the hierarchy of the areas.
     """
     
-    temp_img = convertToGrayscale(img, lccn, debug=debug)
+    temp_img = cutTopOfImage(img, lccn, debug=debug)
+    temp_img = convertToGrayscale(temp_img, lccn, debug=debug)
     temp_img = invert(temp_img, lccn, debug=debug)
-    temp_img = findEdges(temp_img, lccn, debug=debug)
-    temp_img = dilateXDirection(temp_img, lccn, debug=debug)
-    temp_img = closeXDirection(temp_img, lccn, debug=debug)
+    #temp_img = findEdges(temp_img, lccn, debug=debug)
+    temp_img = dilateDirection(temp_img, lccn, debug=debug)
+    temp_img = closeDirection(temp_img, lccn, debug=debug)
+    temp_img = removeNoise(temp_img, lccn, debug=debug)
 
-    _, contours, hierarchy = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return contours, hierarchy
 
 def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
@@ -66,8 +129,8 @@ def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
         x,y,w,h = cv2.boundingRect(contour)
         if currentHierarchy[3] < 0: #outer most contour
             if h > 20 and w > 20:
-                print("writing out word")
-                filepath = os.path.join(directory, '%s_x%s_y%s_w%s_h%s.jp2' % (lccn, x,y,w,h))
+                filepath = os.path.join(directory, '%s_x%s_y%s_w%s_h%s.tiff' % (lccn, x,y,w,h))
+                print("writing out cropped image: [%s]", filepath)
                 if not os.path.exists(filepath):
                     crop_img = img[y:y+h, x:x+w]
                     cv2.imwrite(filepath, crop_img)
@@ -77,16 +140,18 @@ def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
                     cv2.rectangle(boxed,(x,y),(x+w,y+h), green, 3)
 
     if debug:
-        filepath = os.path.join(directory, '%s-contours.jp2' % lccn)
+        filepath = os.path.join(directory, '%s-contours.tiff' % lccn)
         if not os.path.exists(filepath):
             cv2.imwrite(filepath, boxed)
 
 if __name__ == "__main__":
     print("reading test image")
-    img = cv2.imread('sn85038709.jp2')
-    lccn = "sn85038709"
-    create_intermediate_images=true
-    contours, hierarchy = findContours(img, lccn, create_intermediate_images, debug=create_intermediate_images)
+    img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-frontpage.jp2'))
+    lccn = "sn83030212"
+    create_intermediate_images=True
+    if create_intermediate_images and not os.path.exists('debugFiles'):
+        os.makedirs('debugFiles')
+    contours, hierarchy = findContours(img, lccn, debug=create_intermediate_images)
     output_directory = "words"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
