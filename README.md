672bc03a859b70e346796fd76bbaa06dc6aeebf4
John Scancella <blacksmithforlife@gmail.com>
2020-08-15 23:18:28 -0400

    updated program to find title and long lines and remove them. Also updated README to describe the high level flow

--- README.md
+++ README.md
@@ -10,29 +10,25 @@ This repo is an experiment to see if I can use openCV to generate pretty good cr
 #process workflow
 * Stage 1
 	* try and remove banner or long lines because when we morph the image it causes areas to connect that shouldn't and ruins the find contours step
-		* we move banner by knowing that all pictures that have the name 0001.jp2 are the front page and color the top 900 pixels white. This has the effect that they are ignored when we invert the image and run it through various processes in openCV
-		* we remove long lines 
-			* by inverting the picture and running the find contours method of openCV
-			* filter to only those that have a width of 1/2 the page and are skinny in height We need to make sure they are skinny otherwise we will accidentally use the bounding box that covers the entire page...
-			* we color those areas white on the image used for stage 2
-			* filter to only those that have a height of 1/2 the page and are skinny in width. We need to make sure they are skinny otherwise we will accidentally use the bounding box that covers the entire page...
-			* we color those areas white on the image used for stage 2
+		* we find the contours, and any that match a certain width and are in the "top" (can be adjusted) are used to blank out the image in that area.
 * Stage 2 
 	* we take the modified image from stage 1 and morph it to make the white areas (the text) grow in the Y axis. This will allow us to create big blobs that will tell use where the columns are
-	* we try and fill in any small holes (MORPH_CLOSE in openCV speak)
-	* we remove any noise (small white spots randomly, might be from dirt or specks of dust originally)
 	* we find the contours  - this finds the big blobs that are white
 	* we filter the contours to make sure they are the upper most contour in the hierarchy - this usually weeds out the sub contours but not always
 	* We find the bounding box of that contour (because the column may be skewed slightly)
-	* we then crop the image to the size of the bounding box and write it out (due to jp2 no longer being supported we must write it as a tiff)
-	* we then run tesseract on the cropped image - because the image is only the columns, the text makes way more sense then when running it on the whole image
+	* we filter the contours to make sure they meet minimum width and height
+	* we then crop the image to the size of the bounding box WIDTH and the page height and write it out (due to jp2 no longer being supported we must write it as a tiff)
+		* we do the full height because even if there are multiple paragraphs as long as they all match up it doesn't matter and the OCR from Tesseract should be good
+
+* Stage 3
+	* we then run Tesseract on the cropped image - because the image is only the columns, the text makes way more sense then when running it on the whole image
 	* we merge the text together and output it as the text for the whole page
 	* we format the text file so that chronam (or others) can use it?
 	
-* Stage 3a
+* Stage 4a
 	* review the cropped images from stage 2 to make sure they correctly identify the columns in the page and clean/regenerate them if they don't
 	* use the reviewed/regenerated images to train a neural network AI to create the columns automatically
 	
-* Stage 3b
+* Stage 4b
 	* review the text output from stage 2 and fix any mistakes (very labor intensive. look into mechanical turk or crowd sourcing)
 	* use the corrected text and the original image to help train a neural network AI to do OCR for newspapers
--- findText.py
+++ findText.py
@@ -3,12 +3,17 @@ os.environ['OPENCV_IO_ENABLE_JASPER']='True' #has to be set before importing cv2
 import cv2
 import numpy as np 
 
-MIN_COLUMN_WIDTH=450
-MIN_COLUMN_HEIGHT=100
+#Items to tweak if needed for different results
+MIN_COLUMN_WIDTH=450 #what to consider is the minimum width of a column. Anything smaller will be rejected
+MIN_COLUMN_HEIGHT=100 #what to consider is the minimum height of a column. Anything smaller will be rejected
+LINE_THICKNESS = 3 #how thick to make the line around the found contours in the debug output
+PADDING = 10 #padding to add around the found contour(possible column) to help account for image skew and such
+LAST_LINE_OF_TOP_OF_IMAGE = 1000 #only check up to this line for possible lines or title
+LINE_LENGTH_TO_REMOVE = 1000 #any line that is this long or longer will be removed from the top of the image
 
 """
 # Cross-shaped Kernel
->>> cv.getStructuringElement(cv.MORPH_CROSS,(5,5))
+>>> cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
 array([[0, 0, 1, 0, 0],
        [0, 0, 1, 0, 0],
        [1, 1, 1, 1, 1],
@@ -18,7 +23,7 @@ array([[0, 0, 1, 0, 0],
 #kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
 
 #custom kernel that is used to blend together text in the Y axis
-kernel = np.array([
+DILATE_KERNEL = np.array([
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -29,19 +34,41 @@ kernel = np.array([
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0]], dtype=np.uint8)
        
+NOISE_KERNEL = np.array([
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1],
+       [1, 1, 0, 1, 1]], dtype=np.uint8)
+       
+# CLOSE_KERNEL = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
+CLOSE_KERNEL = NOISE_KERNEL
+
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+       
 def blankOutTopOfImage(img, lccn, debug=False):
     """
-    makes the top of the image black in order to remove the newspaper title
+    assume the image has already been inverted and closed,
+    try and find the bounding box around the title or top line and remove it by making it black
     """
-    print("blanking out the top off the image")
-    #TODO find the last row of the title banner
+    print("removing title and or top lines")
     temp_img = np.copy(img)
-    for row in range(0,950): #about right for title page
-    #for row in range(0,250): #about right for other NY tribune pages
-        temp_img[row] = np.full(temp_img[row].shape, 255)
+    contours, hierarchy = cv2.findContours(temp_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    fillRectangle = -1
+    yStart = 0 #never changes because we always want to go from the top
+    for contour in contours:
+        #print("contour: ", contour)
+        x,y,w,h = cv2.boundingRect(contour)
+        
+        if w > LINE_LENGTH_TO_REMOVE and y < LAST_LINE_OF_TOP_OF_IMAGE: #limit to long lines and in the top of the image
+            temp_img = cv2.rectangle(temp_img,(x,0),(x+w,y+h), BLACK, fillRectangle)
+    
     if debug:
         filepath = os.path.join('debugFiles', '%s-blankedout.tiff' % lccn)
         cv2.imwrite(filepath, temp_img)
+    
     return temp_img
 
 def convertToGrayscale(img, lccn, debug=False):
@@ -68,13 +95,7 @@ def removeNoise(img, lccn, debug=False):
     erosion followed by dilation. It is useful in removing noise
     """
     print("remove noise")
-    noiseKernel = np.array([
-       [1, 1, 0, 1, 1],
-       [1, 1, 0, 1, 1],
-       [1, 1, 0, 1, 1],
-       [1, 1, 0, 1, 1],
-       [1, 1, 0, 1, 1]], dtype=np.uint8)
-    temp_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, noiseKernel)
+    temp_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, NOISE_KERNEL)
     if debug:
         filepath = os.path.join('debugFiles', '%s-removeNoise.tiff' % lccn)
         cv2.imwrite(filepath, temp_img)
@@ -90,7 +111,7 @@ def dilateDirection(img, lccn, debug=False):
     It is also useful in joining broken parts of an object. 
     """
     print("applying dilation morph")
-    temp_img = cv2.dilate(img, kernel, iterations=15)
+    temp_img = cv2.dilate(img, DILATE_KERNEL, iterations=15) #the more iterations the more the text gets stretched in the Y axis, 15 seems about right.
    
     if debug:
         filepath = os.path.join('debugFiles', '%s-dilation.tiff' % lccn)
@@ -102,7 +123,7 @@ def closeDirection(img, lccn, debug=False):
     Dilation followed by Erosion. It is useful in closing small holes inside the foreground objects, or small black points on the object
     """
     print("applying closing morph")
-    temp_img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    temp_img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, CLOSE_KERNEL)
     if debug:
         filepath = os.path.join('debugFiles', '%s-close.tiff' % lccn)
         cv2.imwrite(filepath, temp_img)
@@ -116,8 +137,9 @@ def findContours(img, lccn, debug=False):
     
     temp_img = convertToGrayscale(img, lccn, debug=debug)
     temp_img = invert(temp_img, lccn, debug=debug)
-    temp_img = dilateDirection(temp_img, lccn, debug=debug)
     temp_img = closeDirection(temp_img, lccn, debug=debug)
+    temp_img = blankOutTopOfImage(temp_img, lccn, debug=debug)
+    temp_img = dilateDirection(temp_img, lccn, debug=debug)
     temp_img = removeNoise(temp_img, lccn, debug=debug)
 
 #TODO use RETR_LIST instead of RETR_TREE so we don't waste time building a hierarchy?
@@ -130,9 +152,6 @@ def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
     """
     print("creating cropped images")
     boxed = np.copy(img)
-    green = (0, 255, 0)
-    lineThickness = 3
-    padding = 10 #padding to add around the found contour to help account for image skew and such
     
     for component in zip(contours, hierarchy[0]):
         contour = component[0]
@@ -145,42 +164,40 @@ def createTextTiles(img, lccn, contours, hierarchy, directory, debug=False):
             if h > MIN_COLUMN_HEIGHT and w > MIN_COLUMN_WIDTH: #in general columns will be about 450 pixels wide, and we should make sure we aren't getting crazy small ones, 
                                     #so minimum of 100 pixels tall
                 filepath = os.path.join(directory, '%s_x%s_y%s_w%s_h%s.tiff' % (lccn, x,y,w,h))
-                ystart = max(y-padding, 0)
-                yend = min(y+h+padding, img.shape[0])
-                xstart = max(x-padding, 0)
-                xend = min(x+w+padding, img.shape[1])
+                ystart = max(y-PADDING, 0)
+                yend = min(y+h+PADDING, img.shape[0])
+                xstart = max(x-PADDING, 0)
+                xend = min(x+w+PADDING, img.shape[1])
                 crop_img = img[ystart:yend, xstart:xend]
                 if not os.path.exists(filepath):
                     print("writing out cropped image: ", filepath)
                     cv2.imwrite(filepath, crop_img)
                 if debug:
                     #draw this specific bounding box on the copy of the image
-                    cv2.rectangle(boxed,(xstart,ystart),(xend,yend), green, lineThickness)
+                    cv2.rectangle(boxed,(xstart,ystart),(xend,yend), GREEN, LINE_THICKNESS)
                     
 
     if debug:
         filepath = os.path.join('debugFiles', '%s-contours.tiff' % lccn)
-        #cv2.drawContours(boxed, contours, -1, green, lineThickness) #use this to draw all found contours
+        #cv2.drawContours(boxed, contours, -1, green, LINE_THICKNESS) #use this to draw all found contours
         cv2.imwrite(filepath, boxed)
 
 if __name__ == "__main__":
     print("reading test image")
-    img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-frontpage.jp2'))
+    #img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-frontpage.jp2'))
     #img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-page2.jp2'))
     #img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-page3.jp2'))
     #img = cv2.imread(os.path.abspath('./testFiles/newYorkTribune/sn83030212-1841-04-10-page4.jp2'))
+    img = cv2.imread('F:/dlc_gritty_ver01/data/sn83030213/00206530170/1842080101/0001.jp2')
     lccn = "sn83030212"
     create_intermediate_images=True
     if create_intermediate_images and not os.path.exists('debugFiles'):
         os.makedirs('debugFiles')
     
-    #TODO detect if img is a title page and only then blank out the top
-    temp_img = blankOutTopOfImage(img, lccn, debug=create_intermediate_images)
-    
     #TODO split into very rough expected column and run findContours on that keeping only those that are at least 3/4 width of column?
     #that should provide better bounding boxes and ignore tall skinny vertical lines and sub bounding boxes
     
-    contours, hierarchy = findContours(temp_img, lccn, debug=create_intermediate_images)
+    contours, hierarchy = findContours(img, lccn, debug=create_intermediate_images)
     output_directory = "columns"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
